9415201bd00578c9b84de846cc7767ae1887d58f
Calvin Ling <calvinling8@gmail.com>
2020-06-18 13:29:22 -0400

    Add Address and Person models to home app

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import UserProfile
+from .models import UserProfile, Person, Address
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Address)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'first_name', 'last_name', 'cell_phone', 'email')
+
+
+admin.site.register(Person, PersonAdmin)
